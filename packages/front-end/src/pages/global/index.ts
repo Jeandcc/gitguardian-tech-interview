fad327eacf5cf0577df54987b0afc619fe0954b0
Jean Costa <jean.adcc@gmail.com>
2022-02-14 11:22:52 -0300

    feat(front-end): site variable replacement with code

--- packages/front-end/src/pages/global/index.ts
+++ packages/front-end/src/pages/global/index.ts
@@ -1,3 +1,6 @@
+import SiteWideVars from '@/services/SiteWideVars';
+
 import VueUtils from '@/util/Vue';
 
+SiteWideVars.replaceVarsInDom();
 VueUtils.fixWebflowAttributes();
---
+++ packages/front-end/src/services/SiteWideVars.ts
@@ -0,0 +1,32 @@
+interface ISiteWideVars {
+  numberOfDetectors: string;
+}
+
+export default class SiteWideVars {
+  private static getData() {
+    const { siteWideVars } = window as unknown as {
+      siteWideVars?: ISiteWideVars;
+    };
+
+    return siteWideVars;
+  }
+
+  private static getDomTextNodes() {
+    // TODO: Increase coverage based on necessity and watch for bugs
+    return document.querySelectorAll('p,h1,h2,h3,h4,h5,h6,span,div:empty');
+  }
+
+  public static replaceVarsInDom() {
+    const data = this.getData();
+    if (!data) return;
+
+    const textNodes = this.getDomTextNodes();
+
+    textNodes.forEach(node => {
+      node.textContent =
+        node.textContent?.replace(/%(\w*)%/g, (m, key: keyof typeof data) => {
+          return data[key] ? data[key] : '';
+        }) || '';
+    });
+  }
+}
